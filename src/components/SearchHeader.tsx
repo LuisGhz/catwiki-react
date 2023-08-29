9938b9d9b4289c18e9abbe371622d6da408114c6
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-28 18:24:13 -0600

    Create search mobile

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,20 +1,40 @@
+import React from "react";
+import { SearchPortal } from "../SearchPortal";
+import { SearchMobile } from "./SearchMobile";
+import { useGetBreeds } from "../hooks/useGetBreeds";
 import "./SearchHeader.css";
+import { BreedSearcher } from "../models/BreedSearcher.model";
 
 export const SearchHeader = () => {
+  const [isSearchMobileOpen, setIsSearchMobileOpen] = React.useState(false);
+  const { breeds } = useGetBreeds();
+
+  const onSelectBreed = (breed: BreedSearcher) => {
+    console.log(breed);
+  };
+
   return (
-    <header className="header">
-      <h2 className="header__title">CatWiki</h2>
-      <p className="header__text">Get to know more about your cat breed</p>
-      <div className="search">
-        <input
-          className="search__input"
-          type="text"
-          name="search"
-          id="search"
+    <>
+      <SearchPortal isSearchOpen={isSearchMobileOpen}>
+        <SearchMobile
+          setIsSearchMobileOpen={setIsSearchMobileOpen}
+          breeds={breeds}
+          onSelectBreed={onSelectBreed}
         />
-        <span className="search__mobile-text">Search</span>
-        <span className="material-symbols-outlined search__icon">search</span>
-      </div>
-    </header>
+      </SearchPortal>
+      <header className="header">
+        <h2 className="header__title">CatWiki</h2>
+        <p className="header__text">Get to know more about your cat breed</p>
+        <div
+          className="search-mobile"
+          onClick={() => setIsSearchMobileOpen(true)}
+        >
+          <span className="search-mobile__text">Search</span>
+          <span className="material-symbols-outlined search-mobile__icon">
+            search
+          </span>
+        </div>
+      </header>
+    </>
   );
 };

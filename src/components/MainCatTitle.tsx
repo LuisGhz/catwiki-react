af55fe788cc12a928b69d6ebd63a85f6d3f2dde5
Luis Gutiérrez <luisghtz@outlook.com>
2023-08-30 19:07:22 -0600

    implement useNavigate

--- src/components/MainCatTitle.tsx
+++ src/components/MainCatTitle.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 import Cat from '../assets/cat.png';
 import './MainCatTitle.scss'
 
 export const MainCatTitle = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className='main-cat-title'>
+    <div className='main-cat-title' onClick={() => navigate('/')}>
       <h1 className='main-cat-title__title mistery-cat'>CatWiki</h1>
       <img className='main-cat-title__img' src={Cat} alt="cat" />
     </div>
